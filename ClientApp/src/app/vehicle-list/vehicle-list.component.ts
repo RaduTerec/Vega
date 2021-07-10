5929e790284033a6d8943f3e5846dd8fdba02c8e
Radu Terec <Radu.Terec@qiagen.com>
2021-07-10 20:38:00 +0300

    Client-side filtering. Added filter div, onFilterChange and reset button.

--- ClientApp/src/app/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/vehicle-list/vehicle-list.component.ts
@@ -1,3 +1,4 @@
+import { KeyValuePair } from './../models/key-value-pair';
 import { Component, OnInit } from '@angular/core';
 import { Vehicle } from '../models/vehicle';
 import { VehicleService } from '../services/vehicle.service';
@@ -9,12 +10,36 @@ import { VehicleService } from '../services/vehicle.service';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
+  makes: KeyValuePair[];
+  filter: any = {};
   
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+    .subscribe(makes => this.makes = makes);
+
     this.vehicleService.getVehicles()
-    .subscribe(vehicles => this.vehicles = vehicles);
+    .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
   }
 
+  onFilterChange() {
+    var vehicles = this.allVehicles;
+
+    if (this.filter.makeId) {
+      vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+    }
+
+    if (this.filter.modelId) {
+      vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+    }
+
+    this.vehicles = vehicles;
+  }
+
+  resetFilter() {
+    this.filter = {};
+    this.onFilterChange();
+  }
 }
